d2d1a1c76e01510ffaae91ccb2b41b76bdbf97dc
Łukasz Jarzęcki <jarzeckil@gmail.com>
2024-01-11 21:11:15 +0100

    dodanie pliku z makrami znakow dodanie funkcji genBoard i printBoard stworzenie szablonu pliku main.c

---
+++ encoding.h
@@ -0,0 +1,22 @@
+//Space chars unicode codes
+#define WHITE_SQUARE L'█'    
+#define BLACK_SQUARE L' '          
+
+//Frame chars unicode codes
+#define TOP_LEFT L'┌'        
+#define TOP_RIGHT L'┐'       
+#define HORIZONTAL_L L'─'    
+#define VERTICAL_L L'│'      
+#define BOTTOM_LEFT L'└'     
+#define BOTTOM_RIGHT L'┘'    
+
+//Ant chars unicode codes
+#define ARROW_NORTH_BLACK L'△'
+#define ARROW_NORTH_WHITE L'▲'
+#define ARROW_EAST_BLACK L'▷'
+#define ARROW_EAST_WHITE L'▶'
+#define ARROW_SOUTH_BLACK L'▽'
+#define ARROW_SOUTH_WHITE L'▼'
+#define ARROW_WEST_BLACK L'◁'
+#define ARROW_WEST_WHITE L'◀'
+   
