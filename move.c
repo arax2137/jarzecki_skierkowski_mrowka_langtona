0589a68151d9798da5d5a51ce87f63fc4921b96e
pskier <153275518+pskier@users.noreply.github.com>
2024-01-17 18:13:18 +0100

    Update move.c

--- move.c
+++ move.c
@@ -10,92 +10,92 @@ void przejscie(wchar_t **board,int r,int c)
     {
         for (int j = 1; j < c-1; j++)
         {
-            if (board[i][j] == 'u')
+            if (board[i][j] == ARROW_NORTH_WHITE)
             {
-                if (board[i][j + 1] == 's' || board[i][j + 1] == 's')
+                if (board[i][j + 1] == VERTICAL_L || board[i][j + 1] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'c';
-                if (board[i][j+1] == 'b')
-                    board[i][j+1] = 'r';
+                board[i][j] = BLACK_SQUARE;
+                if (board[i][j+1] ==WHITE_SQUARE)
+                    board[i][j+1] = ARROW_EAST_WHITE;
                 else
-                    board[i][j+1] = '>';
+                    board[i][j+1] = ARROW_EAST_BLACK;
                 return;
             }
-           else if (board[i][j] == 'd')
+           else if (board[i][j] == ARROW_SOUTH_WHITE)
             {
-                if (board[i][j-1] == 's' || board[i][j-1] == 's')
+                if (board[i][j-1] == VERTICAL_L || board[i][j-1] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'c';
-                if (board[i][j-1] == 'b')
-                    board[i][j-1] = 'l';
+                board[i][j] = BLACK_SQUARE;
+                if (board[i][j-1] == WHITE_SQUARE)
+                    board[i][j-1] = ARROW_WEST_WHITE;
                 else
-                    board[i][j-1] = '<';
+                    board[i][j-1] = ARROW_WEST_BLACK;
                 return;
             }
-            else if (board[i][j] == 'l')
+            else if (board[i][j] == ARROW_WEST_WHITE)
             {
-                if (board[i-1][j] == 's' || board[i-1][j] == 's')
+                if (board[i-1][j] == VERTICAL_L || board[i-1][j] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'c';
-                if (board[i-1][j] == 'b')
-                    board[i-1][j] = 'u';
+                board[i][j] = BLACK_SQUARE;
+                if (board[i-1][j] == WHITE_SQUARE)
+                    board[i-1][j] = ARROW_NORTH_WHITE;
                 else
-                    board[i-1][j] = '^';
+                    board[i-1][j] = ARROW_NORTH_BLACK;
                 return;
             }
-            else if (board[i][j] == 'r')
+            else if (board[i][j] == ARROW_EAST_WHITE)
             {
-                if (board[i+1][j] == 's' || board[i+1][j] == 's')
+                if (board[i+1][j] == VERTICAL_L || board[i+1][j] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'c';
-                if (board[i+1][j] == 'b')
-                    board[i+1][j] = 'd';
+                board[i][j] =BLACK_SQUARE;
+                if (board[i+1][j] == WHITE_SQUARE)
+                    board[i+1][j] = ARROW_SOUTH_WHITE;
                 else
-                    board[i+1][j] = 'v';
+                    board[i+1][j] = ARROW_SOUTH_BLACK;
                 return;
             }
-            else if (board[i][j] == '^')
+            else if (board[i][j] == ARROW_NORTH_BLACK)
             {
-                if (board[i][j-1] == 's' || board[i][j-1] == 's')
+                if (board[i][j-1] == VERTICAL_L || board[i][j-1] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'b';
-                if (board[i][j-1] == 'b')
-                    board[i][j-1] = 'l';
+                board[i][j] = WHITE_SQUARE;
+                if (board[i][j-1] == WHITE_SQUARE)
+                    board[i][j-1] = ARROW_WEST_WHITE;
                 else
-                    board[i][j-1] = '<';
+                    board[i][j-1] =ARROW_WEST_BLACK;
                 return;
             }
-            else if (board[i][j] == 'v')
+            else if (board[i][j] == ARROW_SOUTH_BLACK)
             {
-                if (board[i][j+1] == 's' || board[i][j+1] == 's')
+                if (board[i][j+1] == VERTICAL_L || board[i][j+1] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'b';
-                if (board[i][j+1] == 'b')
-                    board[i][j+1] = 'r';
+                board[i][j] =WHITE_SQUARE;
+                if (board[i][j+1] == WHITE_SQUARE)
+                    board[i][j+1] = ARROW_EAST_WHITE;
                 else
-                    board[i][j+1] = '>';
+                    board[i][j+1] = ARROW_EAST_BLACK;
                 return;
             }
-            else if (board[i][j] == '<')
+            else if (board[i][j] == ARROW_WEST_BLACK)
             {
-                if (board[i+1][j] == 's' || board[i+1][j] == 's')
+                if (board[i+1][j] == VERTICAL_L || board[i+1][j] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'b';
-                if (board[i+1][j] == 'b')
-                    board[i+1][j] = 'd';
+                board[i][j] = WHITE_SQUARE
+                if (board[i+1][j] == WHITE_SQUARE)
+                    board[i+1][j] = ARROW_SOUTH_WHITE;
                 else
-                    board[i+1][j] = 'v';
+                    board[i+1][j] = ARROW_SOUTH_BLACK;
                 return;
             }
-            else if (board[i][j] == '>')
+            else if (board[i][j] == ARROW_EAST_BLACK)
             {
-                if (board[i-1][j] == 's' || board[i-1][j] == 's')
+                if (board[i-1][j] == VERTICAL_L || board[i-1][j] == HORIZONTAL_L)
                     system("PAUSE");
-                board[i][j] = 'b';
-                if (board[i-1][j] == 'b')
-                    board[i-1][j] = 'u';
+                board[i][j] =WHITE_SQUARE;
+                if (board[i-1][j] == WHITE_SQUARE)
+                    board[i-1][j] = ARROW_NORTH_WHITE;
                 else
-                    board[i-1][j] = '^';
+                    board[i-1][j] = ARROW_NORTH_BLACK;
                 return;
             }
         }
