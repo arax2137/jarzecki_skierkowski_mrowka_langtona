552227419a58ce1510d2f63fa32c9740e761178e
Łukasz Jarzęcki <jarzeckil@gmail.com>
2024-01-17 17:11:23 +0100

    odwrocenie kolorow

--- macro.h
+++ macro.h
@@ -1,5 +1,5 @@
-#define WHITE_SQUARE L'█'    
-#define BLACK_SQUARE L' '          
+#define BLACK_SQUARE L'█'    
+#define WHITE_SQUARE L' '          
 
 #define TOP_LEFT L'┌'        
 #define TOP_RIGHT L'┐'       
@@ -8,12 +8,12 @@
 #define BOTTOM_LEFT L'└'     
 #define BOTTOM_RIGHT L'┘'    
 
-#define ARROW_NORTH_BLACK L'△'
-#define ARROW_NORTH_WHITE L'▲'
-#define ARROW_EAST_BLACK L'▷'
-#define ARROW_EAST_WHITE L'▶'
-#define ARROW_SOUTH_BLACK L'▽'
-#define ARROW_SOUTH_WHITE L'▼'
-#define ARROW_WEST_BLACK L'◁'
-#define ARROW_WEST_WHITE L'◀'
+#define ARROW_NORTH_WHITE L'△'
+#define ARROW_NORTH_BLACK L'▲'
+#define ARROW_EAST_WHITE L'▷'
+#define ARROW_EAST_BLACK L'▶'
+#define ARROW_SOUTH_WHITE L'▽'
+#define ARROW_SOUTH_BLACK L'▼'
+#define ARROW_WEST_WHITE L'◁'
+#define ARROW_WEST_BLACK L'◀'
    
